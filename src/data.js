c5dfe929b1bca3e1ff746aa18bc2b443781aeecc
caperaven <caperaven@gmail.com>
2020-04-07 22:16:22 +0200

    Getting data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export function getData() {
+    const result = [];
+
+    for (let i = 0; i < 10; i++) {
+        result.push({
+            id: i,
+            code: `code ${i}`,
+            isActive: !Math.round(Math.random()),
+            value: getRandomNumber(0, 100),
+            date: getRandomDate()
+        })
+    }
+
+    return result;
+}
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+function getRandomDate() {
+    const start = new Date(2000, 1, 1);
+    const end = new Date();
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+}
